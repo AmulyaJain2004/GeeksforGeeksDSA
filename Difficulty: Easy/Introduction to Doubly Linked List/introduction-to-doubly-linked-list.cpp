68744dcb0ffdef5c2377cde4a7ee1ea4977cfd6d
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-02-21 10:55:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.cpp
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.cpp
@@ -77,15 +77,27 @@ class Solution {
   public:
     Node* constructDLL(vector<int>& arr) {
         // code here
+        // Node* head = new Node(arr[0]);
+        // Node* temp = head;
+        // Node* pretemp = head;
+        // for (int i = 1; i < arr.size(); i++){
+        //     Node* newNode = new Node(arr[i]);
+        //     temp->next = newNode;
+        //     temp = temp->next;
+        //     temp->prev = pretemp;
+        //     pretemp = pretemp->next;
+        // }
+        // return head;
+        
+        // Optimized:
+        if (arr.empty()) return nullptr; //Good Practice to Handle edge case
         Node* head = new Node(arr[0]);
         Node* temp = head;
-        Node* pretemp = head;
         for (int i = 1; i < arr.size(); i++){
             Node* newNode = new Node(arr[i]);
             temp->next = newNode;
+            newNode-> prev = temp;
             temp = temp->next;
-            temp->prev = pretemp;
-            pretemp = pretemp->next;
         }
         return head;
     }
