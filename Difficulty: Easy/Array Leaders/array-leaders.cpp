da58493de69a6bccd06dcd5a14ae2cd9f843c18e
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-07-05 22:29:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -4,11 +4,10 @@ class Solution {
     // Function to find the leaders in the array.
   public:
     vector<int> leaders(vector<int>& arr) {
-        int j = 0; 
         vector<int> new_arr;
         new_arr.push_back(arr[arr.size()-1]);
         for (int i = arr.size()-2; i >= 0 ; i-- ){
-            if (arr[i] >=new_arr[j]){
+            if (arr[i] >=new_arr[0]){
                 new_arr.insert(new_arr.begin(), arr[i]);
             }
         }
